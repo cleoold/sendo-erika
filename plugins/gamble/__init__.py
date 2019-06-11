292127908f562d096f8cf56c4458ffdb95f32998
midori <cleoold@gmail.com>
2019-06-11 22:34:51 +0800

    flip coin

--- plugins/gamble/__init__.py
+++ plugins/gamble/__init__.py
@@ -3,41 +3,63 @@ from nonebot import (CommandSession, IntentCommand, NLPSession, on_command,
                      on_natural_language)
 from nonebot.permission import *
 
-from .diceroll import roll_dice, roll_dice_many
+from utils_bot.typing import Callable, Generator
+
+from .diceroll import *
 
 __plugin_name__ = '试试人品 *NL'
 __plugin_usage__ = f'''feature: 生成随机数
 可用命令：
 扔骰子, 扔骰子 []次
+扔硬币, 扔硬币 []次
 '''
 
+# help session
 @on_command('试试人品', permission=GROUP_MEMBER | SUPERUSER)
 async def try_luck(session: CommandSession):
     await session.send(__plugin_usage__)
 
+class random_ops:
+    @staticmethod
+    async def evaluate(session: CommandSession, f: Callable):
+        times: int = session.get('times')
+        await session.send(f(**( {'times': times} if times else {} )))
+    
+    @staticmethod
+    def nl_proc(session: NLPSession) -> Generator[str, None, None]:
+        argsStripped: str = session.msg_text.strip()
+        words: Generator = posseg.lcut(argsStripped)
+
+        for word in words:
+            if word.flag == 'm':
+                yield word.word
+        yield None
 
 @on_command('扔骰子', aliases=('扔色子', '扔个骰子', '扔个色子'), permission=GROUP_MEMBER | SUPERUSER)
 async def roll_dice_host(session: CommandSession):
-    times: int = session.get('times')
-    if times in ('', '1'):
-        await session.send(roll_dice(pixel=True))
-    else:
-        await session.send(roll_dice_many(times))
+    await random_ops.evaluate(session, roll_dice_many)
+
+
+@on_command('扔硬币', aliases=('扔个硬币', '扔钢镚', '扔个钢蹦'), permission=GROUP_MEMBER | SUPERUSER)
+async def flip_coin_host(session: CommandSession):
+    await random_ops.evaluate(session, flip_coin_many)
+
+
 
 @roll_dice_host.args_parser
+@flip_coin_host.args_parser
 async def _(session: CommandSession):
     argsStripped: str = session.current_arg_text.strip(' \n次')
-    session.state['times'] = argsStripped if argsStripped else ''
+    session.state['times'] = argsStripped if argsStripped else None
+
+
 # EXP
 @on_natural_language(keywords={'骰子', '色子'})
 async def _(session: NLPSession):
-    argsStripped = session.msg_text.strip()
-    words = posseg.lcut(argsStripped)
+    times = next(random_ops.nl_proc(session))
+    return IntentCommand(64.0, '扔骰子', current_arg=times or None)
 
-    times = None
-    for word in words:
-        if word.flag == 'm':
-            times = word.word
-
-    return IntentCommand(64.0, '扔骰子', current_arg=times or '')
-
+@on_natural_language(keywords={'硬币', '钢镚'})
+async def _(session: NLPSession):
+    times = next(random_ops.nl_proc(session))
+    return IntentCommand(64.0, '扔硬币', current_arg=times or None)
--- plugins/gamble/diceroll.py
+++ plugins/gamble/diceroll.py
@@ -3,6 +3,9 @@ import random
 from utils_bot.command_ops import time_limit
 from utils_bot.typing import Any
 
+__all__ = ['roll_dice_many', 
+           'flip_coin_many']
+
 DICE_RESULTS = (
 '0',
 '''
@@ -73,12 +76,14 @@ def roll_dice(pixel:bool=False) -> str:
     else:
         return str(res)
 
-def roll_dice_many(times: Any,
-                   errormsg:str='次数太多扔不过来啦~') -> str:
-    'weird results come if not int...'
+def roll_dice_many(times:Any=1) -> str:
+    'have to assure [times] is in the form of an int...'
     res: str = ''
     try:
         times = int(times)
+        if times == 1:
+            return roll_dice(pixel=True)
+        
         if times > 300000:
             raise TimeoutError
         resDict = {'1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0}
@@ -89,12 +94,30 @@ def roll_dice_many(times: Any,
             res += f'{j}: {resDict[j]}次\n'
         return res.strip()
     except TimeoutError:
-        return errormsg
-    # ?
+        return '次数太多扔不过来啦~'
     except ValueError:
-        if random.choice((0,0,0,0,1)):
-            for j in ('1', '2', '3', '4', '5', '6'):
-                res += f'{j}: {hex(random.randint(0x00, 0xFFFFFFFFFFFF))}次\n'
-            return res.strip()
-        else:
-            return '?'
+        return '?'
+
+
+def flip_coin() -> str:
+    res: int = random.randint(0, 1)
+    return ('反', '正')[res]
+
+def flip_coin_many(times:Any=1) -> str:
+    'have to assure [times] is in the form of an int...'
+    try:
+        times = int(times)
+        if times == 1:
+            return flip_coin()
+        
+        if times > 300000:
+            raise TimeoutError
+        resList = [0, 0]
+        for _ in range(times):
+            r: int = random.randint(0, 1)
+            resList[r] += 1
+        return f'正：{resList[1]} 反：{resList[0]}'
+    except TimeoutError:
+        return '次数太多扔不过来啦~'
+    except ValueError:
+        return '?'
