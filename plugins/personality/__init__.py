29ee6589a81c26002e25a1b65092ae592c9a500d
midori <cleoold@gmail.com>
2019-06-16 00:44:57 +0800

    weather url format

--- plugins/personality/__init__.py
+++ plugins/personality/__init__.py
@@ -4,7 +4,7 @@ from nonebot import CommandSession as CmdS
 from nonebot.permission import *
 
 from utils_bot.command_ops import on_grp_command_ask, on_grp_command_do
-from utils_bot.string_ops import half_none
+from utils_bot.string_ops import half_none, prob_pick
 
 __plugin_usage__ = '一些个性化回复'
 
@@ -20,16 +20,20 @@ async def _(s: CmdS): await s.send(f'16{half_none("！")}')
 @on_grp_command_ask('主人是谁', '你的主人是谁', '谁是你的主人')
 async def _(s: CmdS): await s.send('没有')
 
-@on_grp_command_do('结婚')
+@on_grp_command_ask('你爱我吗', '你爱不爱我', '爱我吗', '爱不爱我')
+async def _(s: CmdS): await s.send('想要获得本小姐的青睐，可不是容易的事情噢~')
+
+@on_grp_command_do('结婚', '嫁给我')
 async def _(s: CmdS): await s.send('绝对不行')
                 # | | | | ?
                 # v v v v
-@on_grp_command_do('娇喘', '淫叫', '浪叫')
+@on_grp_command_do('娇喘', '淫叫', '叫床')
 async def _(s: CmdS):
-    await s.send(
-        ('你怎么了？', '哈？', '哈??', '嗯……',
-        '……怎么突然说这种话',
-        '…哈……哈…唔嗯…………唔哈哈~……啊啊啊')[random.randint(0,5)]
-)
+    reply = ('你怎么了？', '哈？', '哈??', '嗯……',
+             '……怎么突然说这种话',
+             '…哈……哈…唔嗯…………唔哈哈~……啊啊啊',
+             '嗯，咯……！啊啊，呀，啊嗯，呷，呼啊，哈……，啊\n——我们，一起！')
+    prob = (.1, .35, .6, .75, .9, .96, 1)
+    await s.send(prob_pick(reply, prob))
 
 
