9f9a085b615352f9dcae1fc2c0c1c36b043458c0
midori <cleoold@gmail.com>
2020-02-02 22:10:44 -0500

    baidu feed

--- plugins/baidutrend/data_source.py
+++ plugins/baidutrend/data_source.py
@@ -12,21 +12,24 @@ def getTime() -> str:
 
 # a page that always works for the program purpose 
 url: str = "https://www.baidu.com/s?wd=sousuoredian"
-headers: dict = { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36' }
+headers: dict = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
+    # 'Cookie': 'sometimes you need this'
+}
 
-async def get_baidu_trend(*args) -> str:
+async def get_baidu_trend(opt: str) -> str:
     try:
         async with aiohttp.ClientSession() as session:
-            res = await session.get(url, headers=headers, timeout=3, ssl=False)
+            res = await session.get(url, headers=headers, timeout=3)
             soup = bs4.BeautifulSoup(await res.read(), features='lxml')
 
-        titles = soup.select('td table tbody tr td span a')           #|
-        pops = soup.select('td table tbody tr .opr-toplist1-right')   #| place for updates
+        titles = soup.select('.c-table.opr-toplist1-table a')           #|
+        pops = soup.select('.opr-toplist1-right')                       #| place for updates
         length = len(titles)
         if length != len(pops) or length < 5: # one news matches one pop
             raise Exception('The number of entries don\'t match the number of popularities. Something must go wrong.')
         
-        if args[0] != 'all':
+        if opt != 'all':
             titles = titles[:6]
             pops = pops[:6]
             length = 6
