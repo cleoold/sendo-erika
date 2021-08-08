5f7ce8cb9932240bfdc1dce92eda3cdcf0859dce
cleoold <cleoold@gmail.com>
2021-08-08 13:57:42 -0400

    fix now-unusable music api provider to hibiapi demo

--- plugins/song/data_source.py
+++ plugins/song/data_source.py
@@ -1,19 +1,20 @@
+from urllib.parse import quote_plus
+
 import aiohttp
 
 from utils_bot.logging import logger
 from utils_bot.typing import Union
 
-# source: https://github.com/a632079/teng-koa
+# source: https://github.com/mixmoe/HibiAPI
 
-URL_BASE = 'https://v1.hitokoto.cn/nm/search/'
-URL_PARAM = '?type=SONG&offset=0&limit=1'
+URL_FORMAT = 'https://api.obfs.dev/api/netease/search?s={}&search_type=1&limit=1&offset=0'
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'}
 
 async def get_netease_song_id(name: str) -> Union[int, None]:
     try:
         async with aiohttp.ClientSession() as session:
-            res = await session.get(f'{URL_BASE}{name}{URL_PARAM}', headers=headers, timeout=10)
+            res = await session.get(URL_FORMAT.format(quote_plus(name)), headers=headers, timeout=10)
             res.raise_for_status()
             jsn = await res.json()
             return jsn['result']['songs'][0]['id']
